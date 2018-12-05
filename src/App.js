ec24e14a9f1fb262d6d1e32069d2b9199f99845f
Max Stern <stern.maxwell@gmail.com>
2018-12-05 15:41:28 -0600

    navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import AppNavBar from './components/layout/AppNavBar';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <h1>Hello</h1>
-      </div>
+      <Router>
+        <div className="App">
+        <AppNavBar />
+        <div className="container">
+        <h1>Hello</h1>
+        </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/layout/AppNavBar.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class AppNavBar extends Component {
+    render() {
+        return (
+            <nav class="navbar navbar-expand-md navbar-dark bg-primary mb-4">
+                <div class="container">
+                    <Link to="/" className="navbar-brand">
+                    ClientPanel
+                    </Link>
+                    <button 
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarMain"
+                    >
+                    <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collpase navbar-collapse" id="navbarMain">
+                        <ul className="navbar-nav mr-auto">
+                            <li className="nav-item">
+                                <Link to="/" className="nav-link">Dashboard</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        )
+    }
+}
+
+export default AppNavBar;
